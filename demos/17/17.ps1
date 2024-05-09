760ff19f4561239cd75c18a159589edb9c534396
ClaudioESSilva <claudiosil100@gmail.com>
2024-05-09 07:34:10 +0000

    Update demo 17

--- demos/17/17.ps1
+++ demos/17/17.ps1
@@ -6,6 +6,17 @@
 
 $databaseToRefresh = "ToTestRefresh"
 
+# Connect as PRODLogin to 'dbatools1' to see the "PROD" data
+$cred_PROD = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "PRODLogin", $securePassword
+Invoke-DbaQuery -SqlInstance dbatools1 -SqlCredential $cred_PROD -Database $databaseToRefresh -Query "SELECT SUSER_NAME() AS UserName, * FROM dbo.TestTable"
+
+
+# Connect as QALogin to 'dbatools2' to see the "QA" database doesn't have the same data
+$cred_QA = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "QALogin", $securePassword
+Invoke-DbaQuery -SqlInstance dbatools2 -SqlCredential $cred_QA -Database $databaseToRefresh -Query "SELECT SUSER_NAME() AS UserName, * FROM dbo.TestTable"
+
+
+
 # 1 - Export users on destination
 # Export all users from the specific database and its permissions at database-roles and object level
 $usersExport = Export-DbaUser -SqlInstance dbatools2 -Database $databaseToRefresh -Passthru
@@ -29,13 +40,13 @@ Copy-DbaDatabase @copyDatabaseSplat
 
 # Verify the orphan users
 # Verify orphan users
-Get-DbaDbOrphanUser -SqlInstance $dbatools2 -Database $databaseToRefresh
+Get-DbaDbOrphanUser -SqlInstance dbatools2 -Database $databaseToRefresh
 
 # Repair Orphan users
-Repair-DbaDbOrphanUser -SqlInstance $dbatools2 -Database $databaseToRefresh
+Repair-DbaDbOrphanUser -SqlInstance dbatools2 -Database $databaseToRefresh
 
 # Remove Orphan Users
-Remove-DbaDbOrphanUser -SqlInstance $dbatools2 -Database $databaseToRefresh -Verbose
+Remove-DbaDbOrphanUser -SqlInstance dbatools2 -Database $databaseToRefresh -Verbose
 
 
 # Recreate users and grant permissions from the exported command
@@ -49,8 +60,8 @@ Get-DbaDbOrphanUser -SqlInstance dbatools2 -Database $databaseToRefresh -Verbose
 
 # Connect as QALogin and try to select some data
 # Test connect as QALogin and select table where it does not have permissions
-$cred_QA = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "QALogin", "dbatools.IO"
-Invoke-DbaQuery -SqlInstance $dbatools2 -SqlCredential $cred_QA -Database $databaseToRefresh -Query "SELECT TOP 5 SUSER_NAME(), * FROM dbo.ToTestRefresh"
+$cred_QA = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "QALogin", $securePassword
+Invoke-DbaQuery -SqlInstance dbatools2 -SqlCredential $cred_QA -Database $databaseToRefresh -Query "SELECT SUSER_NAME() AS UserName, * FROM dbo.TestTable"
 
 
 
